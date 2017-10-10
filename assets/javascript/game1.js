8a612bd401c95a88350d7d0771c5712af84c12bf
unknown <suhail.purewal@gmail.com>
2017-10-10 16:38:48 -0500

    fixed moving boxes to new divs

--- assets/javascript/game1.js
+++ assets/javascript/game1.js
@@ -48,66 +48,25 @@ var htmlBuild;
 
 // doc ready handler
 $(document).ready(function(){
-// $("#health1").text("Health: " + characters[0].health);
-// $("#health2").text("Health: " + characters[1].health);
-// $("#health3").text("Health: " + characters[2].health);
-// $("#health4").text("Health: " + characters[3].health);
-
-// function(reset) {
-// 	 put a bunch of reset stuff in here 
-// }
-
-$(".resetButton").on("click",function(){
-	reset();
-});
-
-$(".characters").on("click", function(){
-	if (userCharacter === undefined){
-	var placeholder = this.id;
-	userCharacter = characters[placeholder];
-	console.log(userCharacter)
-		$(this).appendTo("yourHero")
-		$(this).removeClass("playableCharacter")
-		$(this).removeClass("characters")
-		$(this).addClass("yourHero")
-		
-		$(".characters").removeClass("playableCharacter")
-		$(".characters").addClass("enemyCharacter")
-		$(".characters").appendTo("enemies")
-		$(".instructionalText").html("Now choose your opponent!");
-	}
-	// 	else if (defenderName === undefined && userCharacter !== undefined){
-		
-	// 	var placeholder = this.id;
-	// 	defenderName = characters[placeholder];
-	// 	console.log(defenderName); 
-	// 	$(this).addClass("hideMe");
-	// 	$(this).removeClass("enemyCharacter"); 
-	// 	$(this).addClass("currentEnemy");
-	// 	$(".instructionalText").html("Let the battle begin!");
-	// }
 
 
-});
-
-
-// no idea what to do to make this work, but i think this is the right idea
-$(".characters").hover(function(){
+$(".character").on("click", function() {
 		var placeholder = this.id;
-		var charinfo = characters[placeholder];
-		$(".instructions").html("<p>" +  charinfo.name + " has : " + charinfo.health + " Health Points and " + charinfo.attack + " Attack Power.</p>");
-	},
-		function(){
-		if(userCharacter===undefined){
-			$(".instructions").html("<p>Choose your Character!</p>");
-		} else if (userCharacter!==undefined && defender===undefined){
-			$(".instructions").html("<p>Pick your Enemies!</p>");
-		} else {
-			$(".instructions").html("<p>FIGHT!</p>");
-		}
-	});
-	
+		var playerSelected = false;
+		userCharacter = characters[placeholder];
+		console.log(userCharacter);
+		$(this).addClass(".pickedName");
+		$(this).removeClass("playableCharacter");
+		$(this).removeClass("character");
+		$(this).appendTo("#yourHero");
+		$(".character").off();
+		$(".character").appendTo("#enemies");
+		$(".character").addClass(".enemies");
+		$(".character").removeClass("character");
+					
+	})
 });
+
 
 
 
